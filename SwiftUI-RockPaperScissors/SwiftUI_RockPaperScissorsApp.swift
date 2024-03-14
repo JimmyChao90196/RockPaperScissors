cd692cdcb540018b699ef4c3e84c4334960415b8
Jimmy <myname90196@gmail.com>
2024-03-14 15:50:04 +0800

    Initial Commit

---
+++ SwiftUI-RockPaperScissors/SwiftUI_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RockPaperScissorsApp.swift
+//  SwiftUI-RockPaperScissors
+//
+//  Created by JimmyChao on 2024/3/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
